5b7cbae9c537703bf120e2c9a80a64c2146b2bd5
Archie <ac11018@ic.ac.uk>
2022-04-18 19:59:46 +0100

    Starting to add specs for genetic representation.

--- src/genetic/representation.clj
+++ src/genetic/representation.clj
@@ -1,4 +1,5 @@
 (ns genetic.representation
+  (:require [clojure.spec.alpha :as s])
   (:require [blif.parser :refer [parse]]))
 
 (defn get-vars [ports]
@@ -6,7 +7,7 @@
 ;; to find edge genes, iterate through variables, find edges where they connect
 
 (defn discover-edge [ports var]
-  {:post [(= 1 (count (filter (partial = :output) (vals %))))]}  ;; Post condition, any edge has only one output
+  {:post [(s/valid? :node/edge %)]}  ;; Post condition, any edge has only one output
   (->> (map-indexed (fn [index port] (if (contains? port var) {index (get port var)})) ports)
        (filter identity)
        (apply merge)))
@@ -18,3 +19,27 @@
   (let [[nodes ports] (parse (slurp f))]
     (let [edges (distinct (ports-to-edges ports))]
       [nodes edges])))
+
+(s/def ::keyword #{:names :input :outputs :latch :constant})
+(s/def :node/type ::keyword)
+(s/def :node/common (s/keys :req [:node/type]))
+(s/def :node/latch-trigger-type #{"re" "fe" "ah" "al" "as"})
+(s/def :node/latch-initial string?)
+(s/def :node/latch (s/merge :node/common (s/keys :req [:node/latch-trigger-type :node/latch-initial])))
+(s/def :node/names-num-inputs (s/and int? pos?))
+;;(s/def :node/names/table )
+;;(s/def :node/names (s/merge :node/common (s/keys :req [:node/names/num-inputs :node/names/table])))
+(s/def :node/constant-value #{:true :false})
+(s/def :node/constant (s/merge :node/common (s/keys :req [:node/constant-value])))
+(s/explain :node/common {:node/type :latch})
+;; TODO perhaps investigate using multi=spec
+
+(s/def ::one-output #(= 1 (count (filter (partial = :output) (vals %)))))
+(s/def ::standard-port #{:output :input :clk})
+(s/def ::indexed-port #(re-matches #":input\d+" (str %)))
+(s/def ::port-keyword (s/or ::standard-port ::indexed-port))
+(s/def :node/edge (s/and (s/map-of int? keyword?) ::one-output))
+
+(s/def :node/edges (s/* :node/edge))
+(s/def :node/nodes (s/* :node/common))
+(s/def :node/in (s/tuple :node/nodes :node/edges))
