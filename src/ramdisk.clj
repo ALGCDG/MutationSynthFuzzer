f444e3ef136ac0c5aa306db3ac09db4a80ff4cb8
Archie <ac11018@ic.ac.uk>
2022-05-08 13:00:15 +0100

    Adding "ramdisk" implementation for Linux.

--- src/ramdisk.clj
+++ src/ramdisk.clj
@@ -9,6 +9,18 @@
 ;;(defn mount-linux [size]
 ;;  (sh "bash" "-c" (format "mount -o size=%d -t tmpfs none %s" size file)))
 
+(defn mount-linux []
+  (log "Creating memory mapped directory in Linux...")
+  (let [file (->> "/dev/shm"
+                  clojure.java.io/file
+                  file-seq
+                  hash
+                  (format "/dev/shm/fuzzmount%X"))]
+    (sh "mkdir" file)
+    (log (format "Creating directory %s..." file))
+    (log "Created memory mapped directory in Linux!")
+    [file nil]))
+
 (defn mount-macos [size]
   (log "Mounting ramdisk in MacOS...")
   (let [numsectors (/ size 512)
@@ -30,17 +42,32 @@
     (log "Mounted ramdisk in MacOS!")
     [file device]))
 
+(defn unmount-linux [file]
+  (log "Removed memory mapped directory in Linux...")
+  (sh "rm" "-rf" file)
+  (log "Removed memory mapped directory in Linux!"))
+
 (defn unmount-macos [[file device]]
   (log "Unmounting ramdisk in MacOS...")
-  (sh "bash" "-c" (format "umount %s" file)
+  (sh "bash" "-c" (format "umount %s" file))
   (sh "bash" "-c" (format "hdiutil detach %s" device))
-  (sh "bash" "-c" (format "rm -r %s" file)))
+  (sh "bash" "-c" (format "rm -r %s" file))
   (log "Unmounted ramdisk in MacOS!"))
+
+(defn unmount [[file device]]
+  (case (System/getProperty "os.name")
+    "Mac OS X" (unmount-macos [file device])
+    "Linux" (unmount-linux file)))
+
+(defn mount [size]
+  (case (System/getProperty "os.name")
+    "Mac OS X" (mount-macos size)
+    "Linux" (mount-linux)))
 
 (defn use-ramdisk "To use ramdisk, provide function which uses the created ramdisk"
   [size func]
-  (let [resource (mount-macos size)]
+  (let [resource (mount size)]
     (try (func resource)
-         (finally (unmount-macos resource)))))
+         (finally (unmount resource)))))
 
 ;;(unmount-macos (mount-macos (* 512 128000)))
