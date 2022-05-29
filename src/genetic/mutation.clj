8ab516cde45bf8dfa0282b88d6af42934e5dfd7f
Archie <ac11018@ic.ac.uk>
2022-05-29 15:06:24 +0100

    Adding more mutation operators and fixing association issue with node and edge lists.

--- src/genetic/mutation.clj
+++ src/genetic/mutation.clj
@@ -7,7 +7,6 @@
 
 (defn find-nodes-indexed [nodes type]
   (filterv (fn [[index node]] (= (get node :type) type)) (enumerate nodes)))
-
 (defn rand-nodetype [seed nodes type] (pure-rand-nth seed (find-nodes-indexed nodes type)))
 
 (defn change-latch-trigger [seed [nodes edges]]
@@ -15,19 +14,19 @@
     (let [[modified-index node] (rand-nodetype node-seed nodes :latch)]
       (let [other-triggers (set/difference #{:re, :fe, :as, :ah, :al} #{(get node :trigger-type)})]
         (let [new-node (assoc node :trigger-type (pure-rand-nth trigger-seed (into [] other-triggers)))]
-          [(assoc nodes modified-index new-node) edges])))))
+          [(assoc (into [] nodes) modified-index new-node) edges])))))
 
 (defn change-latch-initial [seed [nodes edges]]
   (let [[node-seed initial-seed] (generate-seeds seed 2)]
     (let [[modified-index node] (rand-nodetype node-seed nodes :latch)]
       (let [other-initial (set/difference (set (range 4)) #{(get node :initial)})]
         (let [new-node (assoc node :initial (pure-rand-nth initial-seed (into [] other-initial)))]
-          [(assoc nodes modified-index new-node) edges])))))
+          [(assoc (into [] nodes) modified-index new-node) edges])))))
 
 (defn change-constant-value [seed [nodes edges]]
   (let [[modified-index node] (rand-nodetype seed nodes :constant)]
     (let [new-node (assoc node :value (if (= (get node :value) :true) :false :true))]
-      [(assoc nodes modified-index new-node) edges])))
+      [(assoc (into [] nodes) modified-index new-node) edges])))
 
 (defn change-names-flip-term [seed [nodes edges]]
   (let [[node-seed row-seed symbol-seed] (generate-seeds seed 3)
@@ -43,14 +42,14 @@
                                 target-symbol-index
                                 (set/difference (set (CNF-SYMBOLS)) #{target-symbol})))
         new-node (assoc node :table new-table)]
-    [(assoc nodes modified-index new-node) edges]))
+    [(assoc (into [] nodes) modified-index new-node) edges]))
 
 (defn change-names-remove-clause [seed [nodes edges]]
   (let [[modified-index node] (rand-nodetype seed nodes :names)]
     (let [original-table (get node :table)]
       (let [new-table (rest (shuffle original-table))]
         (let [new-node (assoc node :table new-table)]
-          [(assoc nodes modified-index new-node) edges])))))
+          [(assoc (into [] nodes) modified-index new-node) edges])))))
 
 (defn change-names-add-clause [seed [nodes edges]]
   (let [[node-seed sample-seed] (generate-seeds seed 2)]
@@ -60,7 +59,7 @@
           (let [new-clause (take n (pure-sample sample-seed 0.1 (cycle (CNF-SYMBOLS))))]  ;; Note that the probability 0.1 is arbitrary, we are performing a uniform sample (just need to make sure probability is not 1).
             (let [new-table (conj original-table [new-clause])]
               (let [new-node (assoc node :table new-table)]
-                [(assoc nodes modified-index new-node) edges]))))))))
+                [(assoc (into [] nodes) modified-index new-node) edges]))))))))
 
 (defn cut [index col]
   (concat (take index col) (take-last (- (count col) index 1) col)))
@@ -69,7 +68,7 @@
   (let [[node-seed input-seed] (generate-seeds seed 2)
         [target-index target-node] (rand-nodetype node-seed nodes :names)
         target-input (->> target-node :num-inputs range (pure-rand-nth input-seed))]
-    [(assoc nodes
+    [(assoc (into [] nodes)
             target-index
             (assoc target-node
                    :num-inputs (-> target-node :num-inputs (- 1))
@@ -91,7 +90,7 @@
 (defn change-names-add-input [seed [nodes edges]]
   (let [[node-seed symbol-seed edge-seed] (generate-seeds seed 3)
         [target-index target-node] (rand-nodetype node-seed nodes :names)]
-    [(assoc nodes
+    [(assoc (into [] nodes)
             target-index
             (assoc target-node
                    :num-inputs (->> target-node :num-inputs (+ 1))
@@ -195,6 +194,78 @@
              {(count nodes) :output stolen-sink-index stolen-sink-port})
        (conj edges {(count nodes) :output}))]))
 
+(defn add-latch [seed [nodes edges]]
+  (let [[edge-seed sink-seed trigger-seed initial-seed clk-src-seed input-seed] (generate-seeds seed 6)
+        [clk-index clk-edge] (pure-rand-nth clk-src-seed (enumerate edges))
+        [input-index input-edge] (pure-rand-nth input-seed (enumerate edges))
+        [target-index target-edge] (pure-rand-nth edge-seed
+                                                  (filter (fn [[index edge]]
+                                                            (->> edge
+                                                                 vals
+                                                                 (filter #(not= % :output))
+                                                                 count
+                                                                 (< 1)))
+                                                          (enumerate edges)))
+        [stolen-sink-index stolen-sink-port] (pure-rand-nth sink-seed
+                                                            (filter (fn [[index port]]
+                                                                      (not= port :output))
+                                                                    target-edge))]
+    [(conj nodes {:type :latch
+                  :trigger (pure-rand-nth trigger-seed #{"re" "fe" "ah" "al" "as"})
+                  :initial (->> (range 4) (pure-rand-nth initial-seed) str)})
+     (conj (assoc (into [] edges)
+                  clk-index
+                  (assoc clk-edge (count nodes) :clk)
+                  input-index
+                  (assoc input-edge (count nodes) :input)
+                  target-index
+                  (dissoc target-edge stolen-sink-index))
+           {(count nodes) :output stolen-sink-index stolen-sink-port})]))
+
+(defn add-names [seed [nodes edges]]
+  (let [[edge-seed sink-seed num-inputs-seed symbol-seed clause-seed input-seed] (generate-seeds seed 6)
+        num-inputs (->> edges count (range 1) (pure-rand-nth num-inputs-seed))
+        new-sinks (for [[in-seed input-index] (map vector (generate-seeds input-seed num-inputs) (range num-inputs))]
+                (let [[i e] (pure-rand-nth in-seed (enumerate edges))]
+                  [i (assoc e (count nodes) (input-keyword input-index))]))
+        [target-index target-edge] (pure-rand-nth edge-seed
+                                                  (filter (fn [[index edge]]
+                                                            (->> edge
+                                                                 vals
+                                                                 (filter #(not= % :output))
+                                                                 count
+                                                                 (< 1)))
+                                                          (enumerate edges)))
+        [stolen-sink-index stolen-sink-port] (pure-rand-nth sink-seed
+                                                            (filter (fn [[index port]]
+                                                                      (not= port :output))
+                                                                    target-edge))]
+    [(conj nodes {:type :names
+                  :num-inputs num-inputs
+                  :table (let [num-clause (pure-rand-nth clause-seed (range 1 1000))]
+                           (->> (CNF-SYMBOLS)
+                                cycle
+                                (pure-sample symbol-seed 0.1)
+                                (take (* num-clause (+ 1 num-inputs)))
+                                (partition (+ 1 num-inputs))))})
+     (conj (apply (partial assoc (into [] edges)) (flatten new-sinks))
+           {(count nodes) :output stolen-sink-index stolen-sink-port})]))
+
+(defn remove-node [seed [nodes edges]]
+  (let [[node-seed surrogate-seed] (generate-seeds seed 2)
+        [missing-index n] (pure-rand-nth node-seed (enumerate nodes))
+        cleaned-edges (mapv #(dissoc % missing-index) edges)
+        missing-output-edges (filter #(->> % set/map-invert :output not) cleaned-edges)
+        loose-sinks (flatten (mapv #(into [] %) missing-output-edges))
+        suitable-edges (filter #(->> % set/map-invert :output) cleaned-edges)
+        [surrogate-index surrogate-edge] (pure-rand-nth surrogate-seed (enumerate suitable-edges))]
+    [(assoc (into [] nodes) missing-index nil)
+     (if (not (empty? loose-sinks))
+       (assoc (into [] suitable-edges)
+              surrogate-index
+              (apply (partial assoc surrogate-edge) loose-sinks))
+       suitable-edges)]))
+
 (defmacro MUTATIONS [] [change-latch-trigger
                         change-names-remove-clause
                         change-names-add-clause
@@ -212,6 +283,9 @@
                                     `add-output
                                     `add-input
                                     `add-constant
+                                    `add-names
+                                    `add-latch
+                                    `remove-node
                                     `edge-switch-source
                                     `edge-steal-sink]))
 
