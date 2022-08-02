adb971cc12693f5c91e4c607c3ffdab00549b3b9
Archie <ac11018@ic.ac.uk>
2022-08-02 16:22:54 +0100

    Adding option to do synthesis during BLIF to Verilog conversion.

--- src/blif/verilog.clj
+++ src/blif/verilog.clj
@@ -3,26 +3,31 @@
   (:require [blif.compose :refer [generate-blif]])
   (:require [util :refer [log]]))
 
-(defn CONVERT-ARGS [blif-file verilog-file]
-  (format "read_blif %s; write_verilog %s" blif-file verilog-file))
+(defn CONVERT-ARGS [config blif-file verilog-file]
+  (format (if (config :convert-synth)
+            "read_blif %s; synth; write_verilog %s"
+            "read_blif %s; write_verilog %s")
+          blif-file verilog-file))
 
 (defn yosys-convert ;; "Run Yosys subprocess to convert blif file to verilog file."
-  [yosys-path blif-filename verilog-filename]
-  (sh "bash" "-c" (format "%s -p \"%s\" >> convert-log.txt 2> /dev/null"
-                          yosys-path
-                          (CONVERT-ARGS blif-filename verilog-filename))))
+  [config blif-filename verilog-filename]
+  (sh "bash" "-c" (format "%s -p \"%s\""
+                          (config :yosys-path)
+                          (CONVERT-ARGS config blif-filename verilog-filename))))
 
-(defn blif-to-verilog [yosys-path dir name blif]
+(defn blif-to-verilog [config dir name blif]
   (let [blif-file (format "%s/%s.blif" dir name)
         verilog-file (format "%s/%s.v" dir name)]
     (log (format "Writing temporary blif file %s..." blif-file))
     (spit blif-file blif)
-    (log (format "Yosys (%s) converting blif file %s to verilog file %s..." yosys-path blif-file verilog-file))
-    (let [convert-result (yosys-convert yosys-path blif-file verilog-file)]
+    (log (format "Yosys (%s) converting blif file %s to verilog file %s..." (config :yosys-path) blif-file verilog-file))
+    (let [convert-result (yosys-convert config blif-file verilog-file)]
       (sh "rm" blif-file)
       (if (not= (:exit convert-result) 0)
-        (throw (ex-info "BLIF conversion failure" {:type :convert-fail})))
+        (if (= (:exit convert-result) 1)
+          (log "Failed to convert")
+          (throw (ex-info "BLIF conversion failure" {:type :convert-fail :conversion convert-result}))))
       verilog-file)))
 
 (defn genetic-to-verilog [config dir g]
-  (blif-to-verilog (config :yosys-path) dir (format "%X" (hash g)) (generate-blif config g)))
+  (blif-to-verilog config dir (format "%X" (hash g)) (generate-blif config g)))
